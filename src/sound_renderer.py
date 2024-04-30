6b69234c790b50de4c89a870b791c410f2cf4453
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-04-30 20:37:40 +0900

    refactor: use openal type alias

--- src/sound_renderer.py
+++ src/sound_renderer.py
@@ -29,7 +29,7 @@ class SoundRenderer:
         device = alc.alcLoopbackOpenDeviceSOFT(None)
         attrs = [ALC_FORMAT_TYPE_SOFT, ALC_FLOAT_SOFT, ALC_FORMAT_CHANNELS_SOFT,
                  ALC_STEREO_SOFT, ALC_FREQUENCY, SOUND_SAMPLING_RATE, 0]
-        attrs_c = ctypes.c_int * len(attrs)
+        attrs_c = alc.ALCint * len(attrs)
         attrs_c = attrs_c(*attrs)
         context = alc.alcCreateContext(device, attrs_c)
         return SoundRenderer(device, context)
@@ -75,11 +75,11 @@ class SoundRenderer:
 
     def delete_source(self, source_id: int) -> None:
         self.set()
-        al.alDeleteSources(1, ctypes.c_uint(source_id))
+        al.alDeleteSources(1, alc.ALCuint(source_id))
 
     def delete_buffer(self, buffer_id: int) -> None:
         self.set()
-        al.alDeleteBuffers(1, ctypes.c_uint(buffer_id))
+        al.alDeleteBuffers(1, alc.ALCuint(buffer_id))
 
     def close(self) -> None:
         self.set()
@@ -88,23 +88,23 @@ class SoundRenderer:
 
     def is_playing(self, source_id: int) -> bool:
         self.set()
-        state = ctypes.c_int(0)
-        al.alGetSourcei(source_id, al.AL_SOURCE_STATE, ctypes.byref(state))
+        state = alc.ALCint(0)
+        al.alGetSourcei(source_id, al.AL_SOURCE_STATE, state)
         return state.value == al.AL_PLAYING
 
     def al_listener_fv(self, param: int, values: List[float]) -> None:
         self.set()
-        values_arr = (ctypes.c_float * len(values))(*values)
+        values_arr = (alc.ALCfloat * len(values))(*values)
         al.alListenerfv(param, values_arr)
 
     def sample_audio(self) -> np.ndarray[np.float32]:
         self.set()
-        audio_data_type = ctypes.c_float * SAMPLE_SIZE * 2
+        audio_data_type = alc.ALCfloat * SAMPLE_SIZE * 2
         audio_sample = audio_data_type()
         audio_sample_pointer = ctypes.cast(audio_sample, ctypes.c_void_p)
 
         alc.alcRenderSamplesSOFT(self.device, audio_sample_pointer, al.ALsizei(SAMPLE_SIZE))
-        sampled_audio = list(ctypes.cast(audio_sample_pointer, ctypes.POINTER(audio_data_type)).contents)
+        sampled_audio = ctypes.cast(audio_sample_pointer, ctypes.POINTER(audio_data_type)).contents
 
         separated_channel_audio = np.zeros((2, 1024))
         separated_channel_audio[0, :SAMPLE_SIZE] = sampled_audio[0]
