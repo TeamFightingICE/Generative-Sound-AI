26466303556455ad95266c086fc6962fd498fd56
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-05-25 03:23:53 +0900

    feat: setting for host and port

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import asyncio
+import os
 
 import typer
 from dotenv import load_dotenv
 from pyftg.socket.aio.gateway import Gateway
+from typing_extensions import Annotated, Optional
 
 from src.core import SampleSoundGenAI
 from src.utils import setup_logging
@@ -10,8 +12,10 @@ from src.utils import setup_logging
 app = typer.Typer(pretty_exceptions_enable=False)
 
 
-async def start_process():
-    gateway = Gateway()
+async def start_process(host: str, port: int):
+    host = os.environ.get("SERVER_HOST", host)
+    port = int(os.environ.get("SERVER_PORT", port))
+    gateway = Gateway(host, port)
     sound_genai = SampleSoundGenAI()
     gateway.register_sound(sound_genai)
     await gateway.start_sound(keep_alive=True)
@@ -19,8 +23,10 @@ async def start_process():
 
 
 @app.command()
-def main():
-    asyncio.run(start_process())
+def main(
+        host: Annotated[Optional[str], typer.Option(help="Host used by DareFightingICE")] = "127.0.0.1",
+        port: Annotated[Optional[int], typer.Option(help="Port used by DareFightingICE")] = 31415):
+    asyncio.run(start_process(host, port))
 
 
 if __name__ == "__main__":
